e3e613118260dc82d8145d00c959fc1360ada995
Zhodiyn <support-checkout@safemarket.xyz>
2025-06-16 12:45:35 +0400

    Create удостоверение.py

---
+++ src/удостоверение.py
@@ -0,0 +1,11 @@
+DISCORD_BOT_TOKEN = ""
+EMAIL_CHECKER_OPTIONS = [
+    "--no-password-recovery",
+    # "--only-used"
+]
+INSTAGRAM_SESSIONID = ""
+SNUSBASE = "DM DISCORD : 124889922898264897 / hyperdimensionalastroneurophoric"
+NETLAS_API_KEY = "DM DISCORD : 124889922898264897 / hyperdimensionalastroneurophoric"
+GEOSPY_API_KEY = "DM DISCORD : 124889922898264897 / hyperdimensionalastroneurophoric"
+FACECHECK = "DM DISCORD : 124889922898264897 / hyperdimensionalastroneurophoric"
+INTELX_KEY = "DM DISCORD : 124889922898264897 / hyperdimensionalastroneurophoric"
